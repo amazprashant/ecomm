75d3acc40e57e5b9df121125d6cfda9ae860fc4d
amazprashant <amazprashant@gmail.com>
2023-04-18 19:37:33 +0530

    logout feature

--- ecomm-dashboard/src/Header.js
+++ ecomm-dashboard/src/Header.js
@@ -1,7 +1,14 @@
-import {Navbar,Nav} from 'react-bootstrap';
-import{Link} from 'react-router-dom';
+import {Navbar,Nav, NavDropdown} from 'react-bootstrap';
+import{Link,useNavigate} from 'react-router-dom';
 function Header()
 {
+  let user= JSON.parse(localStorage.getItem('user-info'));
+  const navigate = useNavigate();
+  function logOut()
+  {
+    localStorage.clear();
+    navigate("/register");
+  }
     return(
         <div>
         <Navbar bg="dark" variant="dark">
@@ -20,6 +27,14 @@ function Header()
                 </>
             } 
           </Nav>
+          {localStorage.getItem("user-info")?
+          <Nav>
+            <NavDropdown title ={user && user.name}>
+                  <NavDropdown.Item onClick={logOut}>Logout</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          :null 
+}
       </Navbar>
       </div>
     )
