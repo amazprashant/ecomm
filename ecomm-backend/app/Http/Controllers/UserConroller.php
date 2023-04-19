ef64467bcf7642508303d6403f315244634a7e12
sakshichandrakar <sakshichandrakar2911@gmail.com>
2023-04-19 07:56:24 +0530

    Laravel Login Api

--- ecomm-backend/app/Http/Controllers/UserConroller.php
+++ ecomm-backend/app/Http/Controllers/UserConroller.php
@@ -18,4 +18,14 @@ class UserConroller extends Controller
         return $user;
     }
 
+    function login(Request $req)
+    {
+         $user = User::where('email',$req->email)->first();   
+         if(!$user || !Hash::check($req->password,$user->password))
+         {
+            return ["error"=>"Email or Password is not match"];
+         }     
+        return $user;
+    }
+
 }
